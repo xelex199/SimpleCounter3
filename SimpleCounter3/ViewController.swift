d06281b96eb98b918f358c7b96002318ec28df0f
xelex199 <shahpazidi99@mail.ru>
2023-03-29 02:04:20 +0400

    1) Создана кнопка, нажатие которой приводит к увеличению "счетчика" на 1. 2) Созданы две другие кнопки: с икнокой «+» — красного цвета, с иконкой «-» — синего цвета. Соответственно, при нажатии на плюс значение счётчика увеличивается на 1, а при нажатии на минус — уменьшаться на 1. При этом значение счётчика не должно падать ниже нуля. 3) Создана кнопка обнуления, которая при нажатии обнуляет значение счётчика. 4) Создано текстовое поле UITextView, в котором отражена история изменения значения счётчика следующего формата: при старте приложения — «История изменений:» при успешном нажатии на кнопку «+» или «-» — «[дата и время]: значение изменено на -1/+1» при сбросе: — «[дата и время]: значение сброшено» при попытке уменьшить значение счётчика ниже 0 — «[дата и время]: попытка уменьшить значение счётчика ниже 0»

--- SimpleCounter3/ViewController.swift
+++ SimpleCounter3/ViewController.swift
@@ -8,9 +8,83 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count : Int = 0
+    
+    var previousCount : Int = 0
+    
+    func whosBigger(){
+        if count > previousCount{
+            historyOfChanges.insertText("\n [\(Date())]: Значение изменено на +1  \n")
+            previousCount = count
+        }
+        else if count < previousCount{
+            historyOfChanges.insertText("\n [\(Date())]: Значение изменено на -1 \n")
+            previousCount = count
+        }
+    }
+    @IBOutlet weak var plusButton: UIButton!
+    
+    @IBAction func plusCounter(_ sender: Any) {
+        count += 1
+        counter.text = "Значение счетчика : \(count)"
+        whosBigger()
 
+    }
+    
+    
+    @IBOutlet weak var minusButton: UIButton!
+    
+    @IBAction func minusCounter(_ sender: Any) {
+        if count == 0{
+            count += 1
+            historyOfChanges.insertText("\n [\(Date())]: Попытка уменьшить значение счетчика ниже 0 \n")
+            count -= 1
+            previousCount = count
+        }
+        else {
+            count -= 1
+            whosBigger()
+        }
+        counter.text = "Значение счетчика : \(count)"
+    }
+    
+    
+    @IBOutlet weak var zeroCounter: UIButton!
+    
+    @IBAction func makeZeroCounter(_ sender: Any) {
+        count = 0
+        counter.text = "Значение счетчика : \(count)"
+        historyOfChanges.insertText("\n [\(Date())]: Значение сброшено \n")
+        previousCount = count
+    }
+    
+    
+    @IBOutlet weak var counter: UILabel!
+    
+    
+    
+    @IBOutlet weak var countersButton: UIButton!
+    
+    @IBAction func buttonDidTap(_ sender: Any) {
+        count += 1
+        counter.text = "Значение счетчика : \(count)"
+    }
+    
+    
+    @IBOutlet weak var historyOfChanges: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        counter.text = "Значение счетчика : \(count)"
+        plusButton.setTitle("", for: .normal)
+        minusButton.setTitle("", for: .normal)
+        zeroCounter.setTitle("", for: .normal)
+        historyOfChanges.text = " История изменений:"
+        historyOfChanges.isScrollEnabled = true
+        historyOfChanges.isUserInteractionEnabled = true
+        historyOfChanges.isEditable  = false
         // Do any additional setup after loading the view.
     }
 
